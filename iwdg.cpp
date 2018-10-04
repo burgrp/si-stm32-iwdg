dd32e0245f4998ec02f1c7dddc12de4dfb8b9df0
Pavel Burgr <pb@drake.cz>
2018-10-05 01:36:18 +0200

    WIP

--- iwdg.cpp
+++ iwdg.cpp
@@ -10,19 +10,19 @@ namespace iwdg {
             target::IWDG.PR.setPR(4);
             target::IWDG.KR = 0x0000CCCC;
             target::IWDG.KR = 0x0000AAAA;
-            //target::GPIOB.MODER.setMODER(4, 1);
             start(100);
         }
 
         void onTimer() {            
             target::IWDG.KR = 0x0000AAAA;
-            //target::GPIOB.ODR.setODR(4, ~target::GPIOB.IDR.getIDR(4));
     		start(100);
 	    }
 
         void reboot() {
             target::IWDG.KR = 0x00005555;
+            target::IWDG.PR.setPR(0);
             target::IWDG.RLR.setRL(0);
+            for(;;);
         }
 
     };
